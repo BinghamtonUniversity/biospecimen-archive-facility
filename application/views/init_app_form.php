5a420e3d86d900bbd1925d32ecacb1962e8e3f25
Adarsha <adarshakb@adarshakb-Lenovo-U410.(none)>
2013-03-07 10:52:34 -0500

    Issue #5 Changed the views of form
    Added raw HTML content from website to make the all forms looks
    similar to website
    
    Signed-off-by:  <adarshakb@gmail.com>

--- application/views/init_app_form.php
+++ application/views/init_app_form.php
@@ -1,12 +1,102 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Biospecimen Archive Facility - Binghamton University</title>
-    <!-- Bootstrap -->
-    <link href="<?=base_url()?>css/bootstrap.min.css" rel="stylesheet" media="screen">
-  </head>
-  <body>
-    <h1>Initial Application Form.</h1>
+
+
+<!DOCTYPE html
+  PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en" xml:lang="en"><head><title>Binghamton University - Biospecimen Archive Facility - Initial Application Form</title><link rel="stylesheet" type="text/css" href="http://www2.binghamton.edu/css/styles.css" media="all"/><!--[if lt IE 7]>
+  <script defer type="text/javascript" src="http://www2.binghamton.edu/scripts/pngfix.js"></script>
+  <link rel="stylesheet" type="text/css" href="http://www2.binghamton.edu/css/ie_low.css" media="all" />
+  <![endif]--><link media="print" href="http://www2.binghamton.edu/css/print.css" type="text/css" rel="stylesheet"/>
+    <!-- com.omniupdate.properties -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="Keywords" content=""/>
+    <meta name="Description" content=""/>
+    <meta http-equiv="Content-Style-Type" content="text/css"/>
+    <meta http-equiv="imageToolbar" content="no"/>
+    <!-- /com.omniupdate.properties -->
+  <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  </script><script type="text/javascript">
+    var pageTracker = _gat._getTracker("UA-1861349-1");
+    pageTracker._trackPageview();
+  </script></head><body class="departmentPage"><div class="headerOuter"><div class="contentWidth headerInner"><a href="#skipHeader"><img src="http://www2.binghamton.edu/images/common/clear.gif" height="0" width="0" class="skipnav" alt="Skip header content and main navigation"/></a><!-- com.omniupdate.div path="http://www2.binghamton.edu/biospecimen-archive-facility/forms/logo.html" label="logo" --><!-- com.omniupdate.div path="/z-omniupdate/fakes/logo.html" label="logo" -->
+
+<img src="http://www2.binghamton.edu/images/common/bgHeader.png" id="logoBackground" alt="" />
+<img class="logoLink" src="http://www2.binghamton.edu/images/siteLogos/logo-biospecimen-archive-facility.gif" alt="Binghamton University, State University of New York - Biospecimen Archive Facility" usemap="#Map" />
+<map name="Map" id="Map">
+  <area shape="rect" coords="0,0,210,66" href="http://www2.binghamton.edu//index.html" alt="" />
+  <area shape="rect" coords="220,0,407,66" href="http://www2.binghamton.edu//biospecimen-archive-facility/index.html" alt="" />
+</map>
+<!-- /com.omniupdate.div --><!-- /com.omniupdate.div --><!-- com.omniupdate.div path="/inc/hdrQuickLinksDepartment.shtml" label="hdrQuickLinksDepartment" --><script src="https://www.google.com/jsapi?key=ABQIAAAA3ia0NPE98EwrkgLZHTSkgxTXO7ZdFP8GFbr4e1voZqr2tFi3GBQN00cLkqcQU9y6_jIvQSyed39Wfg">
+
+</script>
+<script> 
+    //<![CDATA[
+    if (typeof jQuery == 'undefined') {
+        google.load("jquery", "1.6");
+    }
+    //]]>
+</script>
+<script src="http://www2.binghamton.edu/inc/header-search.min.js">
+
+</script>
+
+<form action="http://www2.binghamton.edu/results.html" id="cse-search-box">
+  <div class="searchControls">
+    <input type="hidden" name="cx" value="013262338783433875854:7stszgiris8" />
+    <input type="hidden" name="cof" value="FORID:11" />
+    <input type="hidden" name="ie" value="UTF-8" />
+    <input type="text" name="q" size="31" class="searchBox" value="Search" onclick="this.select();" />
+    <img src="http://www2.binghamton.edu/images/common/search.png" class="searchButton" alt="Search Button"/>
+  </div>
+</form>
+
+<ul class="inlineList quickLinks" style="padding-top: 10px;">
+  <li><a href="http://m.binghamton.edu/" title="Mobile">Mobile</a></li>
+  <li>|</li>
+  <li><a href="http://my.binghamton.edu" title="My Binghamton">My Binghamton</a></li>
+  <li>|</li>
+  <li><a href="http://www.binghamton.edu/admission.html" title="Apply">Apply</a></li>
+  <li>|</li>
+  <li><a href="/a-to-z/index.html" title="A-Z Index">A-Z Index</a></li>
+</ul>
+
+<div class="searchRadioButtons">
+    <input id="search-filters1" name="tab" value="" checked="checked" type="radio">
+  <label for="search-filters1">Pages</label>
+  <input id="search-filters2" name="tab" value="people" type="radio">
+  <label for="search-filters2">People</label>
+</div>
+<!-- /com.omniupdate.div --><!-- com.omniupdate.div path="/inc/hdrAudienceNavDepartment.shtml" label="hdrAudienceNavDepartment" -->    <ul class="inlineList audienceNav">
+      <li><a href="http://www.binghamton.edu/future-students.html" title="Future Students">Future Students</a></li>
+      <li>|</li>
+      <li><a href="http://www.binghamton.edu/current-students.html" title="Current Students">Current Students</a></li>
+      <li>|</li>
+      <li><a href="http://www.binghamton.edu/visitors-and-community.html" title="Visitors &amp; Community">Visitors &amp; Community</a></li>
+      <li>|</li>
+      <li><a href="http://www.binghamton.edu/alumni/" title="Alumni">Alumni</a></li>
+      <li>|</li>
+      <li><a href="http://www.binghamton.edu/parents.html" title="Parents">Parents</a></li>
+      <li>|</li>
+      <li><a href="http://www.binghamton.edu/faculty-and-staff.html" title="Faculty &amp; Staff">Faculty &amp; Staff</a></li>
+    </ul>
+<!-- /com.omniupdate.div --></div></div><div class="pageWidth"><div class="mainNavOuter"><!-- com.omniupdate.div path="/biospecimen-archive-facility/forms/hnav.html" label="hnav" --><!-- com.omniupdate.editor csspath="/z-omniupdate/edit/department-header/hnav.css" width="1200" -->
+<ul class="inlineList mainNav">
+  <li class="first"><a title="Item One" href="#">Item One</a></li>
+  <li><a title="Item Two" href="#">Item Two</a></li>
+  <li><a title="Item Three" href="#">Item Three</a></li>
+  <li><a title="Item Four" href="#">Item Four</a></li>
+  <li><a title="Item Five" href="#">Item Five</a></li>
+  <li><a title="Item Six" href="#">Item Six</a></li>
+  <li><a title="Item Seven" href="#">Item Seven</a></li>
+  <li><a title="Item Eight" href="#">Item Eight</a></li>
+  <li><a title="Item Nine" href="#">Item Nine</a></li>
+  <li class="last"><a title="Item Ten" href="#">Item Ten</a></li>
+</ul><!-- /com.omniupdate.div --></div></div><div class="contentWidth bodyContent"><div class="contentBanner"/><div class="content contentMinimal"><div class="bcOuter"><!-- com.omniupdate.div path="/z-omniupdate/fakes/breadcrumb.html" label="breadcrumb" --><ul class="bc inlineList"><li><a href="/biospecimen-archive-facility/">biospecimen archive facility</a></li><li>|</li><li><a href="/biospecimen-archive-facility/forms/">forms</a></li></ul><!-- /com.omniupdate.div --></div>
+    <!-- com.omniupdate.div label="content" group="Everyone" button="787" break="break" -->
+    <!-- com.omniupdate.editor csspath="/z-omniupdate/edit/department-content2/content.css" cssmenu="/z-omniupdate/edit/edit-main.txt" width="960" -->
+    <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+</div><h1>Initial Application Form.</h1>
     <?php 
     $errors = validation_errors(); 
     $errors2 = $this->upload->display_errors();
@@ -105,6 +195,37 @@
       </table>
     </form>
     <script src="<?=base_url()?>js/jquery.js"></script>
-    <script src="<?=base_url()?>js/bootstrap.min.js"></script>
-  </body>
-</html>
+    <script src="<?=base_url()?>js/bootstrap.min.js"></script>   <!-- /com.omniupdate.div -->
+  </div></div><!-- com.omniupdate.div path="/z-omniupdate/fakes/footer.html" label="footer" --><!-- begin footer -->
+<div class="contentWidth footerOuter">
+    <div class="footerLeft" style="width: 340px; padding-left: 20px;">
+    <div class="addlInfo">
+    <div style="margin-bottom: 5px"><em>Connect with Binghamton:</em></div>
+    <a href="http://twitter.com/binghamtonu">
+            <img style="display: inline;" src="http://www2.binghamton.edu/images/twitter-icon.png" alt="Twitter icon links to Binghamton University's Twitter page"/>
+          </a>
+
+          <a href="http://www.youtube.com/user/BinghamtonUniversity">
+              <img style="display: inline; padding-left: 8px" src="http://www2.binghamton.edu/images/youtube-icon.png" alt="YouTube icon links to Binghamton University's YouTube page"/>
+          </a>
+          <a href="http://www.facebook.com/pages/Binghamton-NY/Binghamton-University/51791915551">
+              <img style="display: inline; padding-left: 8px" src="http://www2.binghamton.edu/images/facebook-icon.png" alt="Facebook icon links to Binghamton University's Facebook page"/>
+          </a>
+          <a href="http://pinterest.com/binghamtonu/"><img style="display: inline; padding-left: 9px" src="http://www2.binghamton.edu/images/pinterest.png" alt="Pinterest icon links to Binghamton University's Pinterest page" width="32" height="32" /></a>
+      
+    </div>
+  </div>
+    <div class="footerRight" style="width: 570px; padding-right: 20px; line-height: 180%;">
+    <div class="copyright" style="text-align: right">&copy; 2013 Binghamton University, State University of New York<br />
+        <a href="http://www2.binghamton.edu/magazine/">Binghamton Magazine</a> | <a href="http://www2.binghamton.edu/inside/">Inside Binghamton</a> | <a href="http://www.binghamton.edu/photos">Daily Photo</a><br />
+        <a href="http://www.binghamton.edu/about/contact-us.html">Contact Us</a> | <a href="http://www.telecom.binghamton.edu/directory/directory.search">Directory</a></div>
+    <div class="feedsNetworks"> 
+    
+    <div class="shareLinks">         
+         
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end footer -->
+<!-- /com.omniupdate.div --><!-- com.omniupdate.ob --><p class="dired"><a href="http://www.omniupdate.com/oucampus-binghamton/de.jsp?user=Migration&amp;site=binghamton&amp;path=%2Fbiospecimen-archive-facility%2Fforms%2Findex.pcf">Last Updated: 3/5/13</a></p><!-- /com.omniupdate.ob --></body></html>

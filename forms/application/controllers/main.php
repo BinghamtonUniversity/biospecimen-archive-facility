64d33c0783f40f7656969718d2f013a0a328bf95
Adarsha <adarshakb@adarshakb-Lenovo-U410.(none)>
2013-03-22 09:04:51 -0400

    Issue #9 FIX
    Changed the max length of title from 10 to 100
    
    Signed-off-by:  <adarshakb@gmail.com>

--- forms/application/controllers/main.php
+++ forms/application/controllers/main.php
@@ -22,7 +22,7 @@ class Main extends CI_Controller {
 
 		$this->form_validation->set_rules('fname','First-name','xss_clean|trim|required|max_length[100]');
 		$this->form_validation->set_rules('lname','Last-name','xss_clean|trim|required|max_length[100]');
-		$this->form_validation->set_rules('tname','Title','xss_clean|trim|max_length[10]');
+		$this->form_validation->set_rules('tname','Title','xss_clean|trim|max_length[100]');
 		$this->form_validation->set_rules('institution','Institution name','xss_clean|trim|required|max_length[500]');
 		$this->form_validation->set_rules('emailid','Email ID','xss_clean|trim|required|max_length[150]|valid_email');
 		$this->form_validation->set_rules('phno','Phone number','xss_clean|trim|required|max_length[20]');
@@ -120,7 +120,7 @@ class Main extends CI_Controller {
 
 		$this->form_validation->set_rules('fname','First-name','xss_clean|trim|required|max_length[100]');
 		$this->form_validation->set_rules('lname','Last-name','xss_clean|trim|required|max_length[100]');
-		$this->form_validation->set_rules('tname','Title','xss_clean|trim|max_length[10]');
+		$this->form_validation->set_rules('tname','Title','xss_clean|trim|max_length[100]');
 		$this->form_validation->set_rules('institution','Institution name','xss_clean|trim|required|max_length[500]');
 		$this->form_validation->set_rules('emailid','Email ID','xss_clean|trim|required|max_length[150]|valid_email');
 		$this->form_validation->set_rules('phno','Phone number','xss_clean|trim|required|max_length[20]');
